063fe762026d27abdc52c03a140d6fb67df7f83a
Hitesh Tare <hiteshtare@gmail.com>
2024-07-04 12:06:40 +0530

    Added Dynamic link test for Redirection

---
+++ cypress/e2e/urlTest.cy.ts
@@ -0,0 +1,45 @@
+//Import test data for Books
+const testDataForURL = require("../fixtures/teachingsLibrary_url.json");
+
+describe("Teaching library", () => {
+  describe.skip("URL: Test", () => {
+    testDataForURL.forEach((testCase: any, index: number) => {
+      it(`#${index + 1} URL with Title:${
+        testCase["YSS New URL (Redirection TO)"]
+      } should give 200 OK status`, () => {
+        cy.request("HEAD", `${testCase["URLs for testing before redirection"]}`)
+          .its("status")
+          .should("eq", 200);
+
+        cy.log(
+          `#${index + 1} URL with Title: ${
+            testCase["YSS New URL (Redirection TO)"]
+          }`
+        );
+      });
+    });
+  });
+
+  describe("URL: Redirection test", () => {
+    const urlStringMatch = "/teachings-library/";
+
+    testDataForURL.forEach((testCase: any, index: number) => {
+      it(`#${index + 1} Blog url with Title:${
+        testCase["YSS Old URL (Redirection FROM)"]
+      } should redirect to Teaching Library url`, () => {
+        const urlRedirectionFrom = testCase["YSS Old URL (Redirection FROM)"];
+        cy.visit(urlRedirectionFrom);
+
+        cy.url().should("include", urlStringMatch);
+
+        cy.log(
+          `#${index + 1} Blog url with Title: ${
+            testCase["YSS New URL (Redirection FROM)"]
+          }`
+        );
+        
+        cy.screenshot({ capture: "viewport" });
+      });
+    });
+  });
+});
