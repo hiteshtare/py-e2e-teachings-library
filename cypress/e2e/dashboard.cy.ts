0d71ac74644a767269d4be3bd76bec2c9d482f37
Hitesh Tare <hiteshtare@gmail.com>
2024-07-04 17:48:43 +0530

    WIP - Pagination testing

--- cypress/e2e/dashboard.cy.ts
+++ cypress/e2e/dashboard.cy.ts
@@ -13,7 +13,7 @@ describe("Dashboard page", () => {
     cy.visit("/teachings-library");
   });
 
-  describe("Item card", () => {
+  describe.skip("Item card", () => {
     it("card: Image - should be clickable", () => {
       checkStatus200ForLink(dashboardPages.btnCardTitle);
     });
@@ -22,6 +22,64 @@ describe("Dashboard page", () => {
     });
     it("card: Author - should be clickable", () => {
       checkStatus200ForLink(dashboardPages.btnCardTitle);
+    });
+  });
+
+  const goToNextPage = () => {
+    cy.get("@nextButton")
+      .invoke("attr", "disabled")
+      .then((disabled) => {
+        cy.get("@nextButton").should('exist');
+        if (disabled === "disabled") {
+          cy.get("@nextButton").should("have.attr", "disabled");
+        } else {
+          cy.get("@nextButton").click().then(goToNextPage);
+        }
+      });
+  };
+
+  function maybeClickNext(page = 1) {
+    // the Next button is always present
+    // let's get its attribute "disabled"
+
+    let btnNext = "";
+    if (page === 1) {
+      btnNext = ".prev-next > .jet-filters-pagination__link";
+    } else {
+      btnNext = ".next > .jet-filters-pagination__link";
+    }
+
+    cy.get(btnNext, { log: false })
+      .invoke({ log: false }, "attr", "disabled")
+      .then((disabled) => {
+        if (disabled === "disabled") {
+          cy.log("Last page!");
+        } else {
+          cy.log(`Page ${page}`);
+          // not the end yet, sleep half a second for clarity,
+          // click the button, and recursively check again
+          cy.get(btnNext)
+            .click()
+            .then(() => maybeClickNext(page + 1));
+        }
+      });
+  }
+
+  describe("Pagination", () => {
+    it("clicks the Next button until we get to the last page", () => {
+      cy.contains("Next").as("nextButton").click({force:true});
+      goToNextPage();
+
+      // the HTML table on the page is paginated
+      // can you click the "Next" button until
+      // we get to the very last page?
+      // button selector ".prev-next > .jet-filters-pagination__link"
+
+      // maybeClickNext();
+
+      // cy.log("**confirm we are on the last page**");
+      // cy.get(".prev-next > .jet-filters-pagination__link").should("be.disabled");
+      // cy.get("[value=last]").should("be.disabled");
     });
   });
 
